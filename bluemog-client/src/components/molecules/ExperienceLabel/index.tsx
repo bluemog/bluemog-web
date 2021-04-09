66be92d665f18eb9c7712e6f8c8455a80e7636f4
JSKeum <jskeum2512@gmail.com>
2021-04-09 17:19:23 +0900

    fix ExperienceLabel emotion

--- bluemog-client/src/components/molecules/ExperienceLabel/index.tsx
+++ bluemog-client/src/components/molecules/ExperienceLabel/index.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { css } from '@emotion/react';
 
 interface Props {
-    children: React.ReactNode;
-    backgroundColorInHex: string;
+    children?: React.ReactNode;
+    backgroundColorInHex?: string;
+    font?: string;
 }
 
-function ExperienceLabel({ backgroundColorInHex, children }: Props) {
-  return (
-    <span css={css`
+function ExperienceLabel({ backgroundColorInHex = 'fe03ee', font = 'Noto Sans KR', children }: Props) {
+  const labelStyle = css`
+    display: inline-block;
     padding: 16px;
     background: #${backgroundColorInHex};
-  `}
-    >
+    font-family: ${font}, sans-serif
+  `;
+
+  return (
+    <span css={labelStyle}>
       {children}
     </span>
   );
