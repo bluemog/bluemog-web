97ebb5e66fa3db834eee802f8358152bac579acc
9ummy <godante27@gmail.com>
2021-04-10 16:51:52 +0900

    Fix css detail of IntroductionWrapper molecule

--- bluemog-client/src/components/molecules/IntroductionWrapper/index.tsx
+++ bluemog-client/src/components/molecules/IntroductionWrapper/index.tsx
@@ -4,14 +4,23 @@ import { css } from '@emotion/react';
 interface Props {
     children?: React.ReactNode;
     backgroundColorInHex?: string;
-    textColorInHex?: string;
     font?: string;
+    fontSize?: string;
     width?: string;
     height?: string
+    margin?: string;
+    lineClamp?: number;
 }
 
 function IntroductionWrapper({
-  backgroundColorInHex = 'edf5fa', font = 'Noto Sans KR', children, width = '310px', height = '320px',
+  backgroundColorInHex = 'edf5fa',
+  font = 'Noto Sans KR',
+  fontSize = '18px',
+  children,
+  width = '310px',
+  height = '320px',
+  margin = '10px 20px 5px 20px',
+  lineClamp = 8,
 }: Props) {
   const wrapperStyle = css`
     padding: 10px;
@@ -24,15 +33,15 @@ function IntroductionWrapper({
   const textStyle = css`
     color: #1684b5;
     font-family: ${font}, sans-serif;
-    font-size: 18px;
+    font-size: ${fontSize};
     line-height: 2;
     letter-spacing: 0.65px;
-    margin: 10px 20px 5px 20px;
+    margin: ${margin};
 
     overflow: hidden;
     text-overflow: ellipsis;
     display: -webkit-box;
-    -webkit-line-clamp: 8;
+    -webkit-line-clamp: ${lineClamp};
     -webkit-box-orient: vertical;
     word-wrap:break-word; 
   `;
