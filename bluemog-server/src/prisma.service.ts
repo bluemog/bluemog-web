574dc3d59e054ab729dc13306802ccda38e687bc
JSKeum <jskeum2512@gmail.com>
2021-05-20 12:30:13 +0900

    add query log middleware to Prisma

--- bluemog-server/src/prisma.service.ts
+++ bluemog-server/src/prisma.service.ts
@@ -5,11 +5,43 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'stdout',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
+    });
+
+    this.setLogging();
+  }
   async onModuleInit() {
     await this.$connect();
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
+  }
+
+  async setLogging() {
+    // Prisma Type 관련 이슈가 있어서 임시 방편으로 any를 썼습니다
+    this.$on('query' as any, (e: any) => {
+      console.log('Query: ' + e.query);
+      console.log('Duration: ' + e.duration + 'ms');
+    });
   }
 }
