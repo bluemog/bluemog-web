57709126614cf62c34d5e69aef518dde68bebe7d
9ummy <godante27@gmail.com>
2021-05-24 01:42:36 +0900

    add user delete, update operation

--- bluemog-server/src/user/user.controller.ts
+++ bluemog-server/src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Delete,
+  Param,
+  Patch,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { User as UserModel } from '@prisma/client';
 import { UserCreateDto } from './Dto/user.dto';
@@ -15,5 +23,21 @@ export class UserController {
   @Post()
   async signup(@Body() userCreateDto: UserCreateDto): Promise<UserModel> {
     return this.userService.createUser(userCreateDto);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: number): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: number,
+    @Body() updateData,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: updateData,
+    });
   }
 }
